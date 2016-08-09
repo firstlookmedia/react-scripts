3b4b16746bc246cd39545b798fa03b35a0fad742
Tom Conroy <tom@conroy.com.au>
2016-08-09 11:17:45 -0400

    add test config

---
+++ scripts/test.js
@@ -0,0 +1,18 @@
+const spawn = require('cross-spawn');
+const path = require('path');
+
+const testConfig = path.resolve(__dirname, '../config/webpack.test.js');
+const mochaWebpack = path.resolve(__dirname, '../node_modules/.bin/mocha-webpack');
+const setup = path.resolve(__dirname, '../lib/testSetup.js');
+
+spawn(
+  'node',
+  [
+    mochaWebpack,
+    '--colors',
+    '--webpack-config', testConfig,
+    '--require', setup,
+    '**/__spec.js',
+  ],
+  { stdio: 'inherit' }
+);
