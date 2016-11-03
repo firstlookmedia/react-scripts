f693336b8410e18874e8beba22222dbca52fc6fa
Tom Conroy <tom@conroy.com.au>
2016-11-03 18:29:47 -0400

    mock Relay in tests

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack');
+const path = require('path');
 const defaults = require('./webpack.defaults');
 const externals = require('webpack-node-externals');
 
 module.exports = Object.assign({}, defaults, {
   target: 'node',
-  externals: [externals()],
+  externals: [externals({
+    whitelist: ['react-relay'], // to make the alias work
+  })],
   plugins: defaults.plugins.concat([
     new webpack.BannerPlugin(
       'require("source-map-support").install({ environment: "node" });',
@@ -12,4 +15,10 @@ module.exports = Object.assign({}, defaults, {
     ),
   ]),
   devtool: 'source-map',
+  resolve: Object.assign({}, defaults.resolve, {
+    alias: {
+      'react-relay': path.resolve(__dirname, '../lib/mockRelay.js'),
+      'real-react-relay': path.resolve('node_modules/react-relay'),
+    },
+  }),
 });
