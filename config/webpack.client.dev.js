fa47c8120c975ab2276a9dfefae1beeb19dfa73c
Tom Conroy <tom@conroy.com.au>
2016-11-08 10:49:30 -0500

    use fully-fledged source-maps in dev

--- config/webpack.client.dev.js
+++ config/webpack.client.dev.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const defaults = require('./webpack.defaults');
 
 const config = Object.assign({}, defaults, {
-  devtool: 'cheap-module-source-map',
+  devtool: 'source',
   module: Object.assign({}, defaults.module, {
     loaders: defaults.module.loaders.map(loader => {
       if (loader.name === 'css') {
