d325cca3c5f7feee35ea75e395c6005d1de9824c
Tom Conroy <tom@conroy.com.au>
2017-03-03 11:31:16 -0500

    clean up, revert to stage-0

--- config/webpack.defaults.js
+++ config/webpack.defaults.js
@@ -29,7 +29,7 @@ module.exports = {
         presets: [
           'babel-preset-react',
           'babel-preset-es2015',
-          'babel-preset-stage-2',
+          'babel-preset-stage-0',
         ].map(require.resolve),
         plugins: [
           'babel-plugin-transform-runtime',
