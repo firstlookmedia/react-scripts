68572a55852c211a107012f7f618db00abf28b21
Maxwell Newlands <maxwell.newlands@firstlook.org>
2017-07-27 12:14:41 -0400

    Update Relay mock

--- scripts/utils/__mocks__/react-relay.js
+++ scripts/utils/__mocks__/react-relay.js
@@ -1,9 +1,49 @@
-const Relay = require('react-relay');
+const Relay = jest.genMockFromModule('react-relay');
+
+class Mutation extends Relay.Mutation {
+  _resolveProps(props) {
+    this.props = props;
+  }
+}
+
+class MockStore {
+  reset() {
+    this.successResponse = undefined;
+  }
+
+  succeedWith(response) {
+    this.reset();
+    this.successResponse = response;
+  }
+
+  failWith(response) {
+    this.reset();
+    this.failureResponse = response;
+  }
+
+  update(callbacks) {
+    if (this.successResponse) {
+      callbacks.onSuccess(this.successResponse);
+    } else if (this.failureResponse) {
+      callbacks.onFailure(this.failureResponse);
+    }
+    this.reset();
+  }
+
+  commitUpdate(mutation, callbacks) {
+    return this.update(callbacks);
+  }
+
+  applyUpdate(mutation, callbacks) {
+    return this.update(callbacks);
+  }
+}
 
 module.exports = {
   QL: Relay.QL,
-  Mutation: Relay.Mutation,
+  Mutation,
   Route: Relay.Route,
   RootContainer: () => null,
   createContainer: (component) => component,
+  Store: new MockStore(),
 };
