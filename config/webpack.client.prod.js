803b494e2de8a1e9c46f2116ee6ebde07a05c997
Tom Conroy <tom.conroy@firstlook.org>
2018-04-18 14:47:22 -0400

    remove ExtractTextWebpackPlugin (using MiniCSSExtractPlugin)

--- config/webpack.client.prod.js
+++ config/webpack.client.prod.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const merge = require('webpack-merge');
@@ -24,7 +23,6 @@ module.exports = merge.smart({
   plugins: [
     new ManifestPlugin({ fileName: 'manifest.json' }),
     new ManifestPlugin({ fileName: `manifest.${Date.now()}.json` }),
-    new ExtractTextPlugin('[md5:contenthash:hex:20].css'),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"',
     }),
