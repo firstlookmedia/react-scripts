09cb82aaab6de895683bac9e452fc289afd99fd3
Tom Conroy <tom.conroy@firstlook.org>
2018-04-30 11:07:45 -0400

    fix babel-jest config

--- scripts/utils/babelTransform.js
+++ scripts/utils/babelTransform.js
@@ -1,7 +1,14 @@
 const babelJest = require('babel-jest');
 const config = require('../../config/webpack.defaults.js');
 
-module.exports = babelJest.createTransformer(
-  // use babel config defined in webpack config
-  config.module.loaders.find(l => l.loader === 'babel-loader').query
-);
+module.exports = babelJest.createTransformer({
+  passPerPreset: true,
+  presets: [
+    'babel-preset-react',
+    'babel-preset-env',
+    'babel-preset-stage-0',
+  ],
+  plugins: [
+    'babel-plugin-transform-runtime',
+  ],
+});
