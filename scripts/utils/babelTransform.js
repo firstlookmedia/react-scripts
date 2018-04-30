20e893379ea14283bb9c1fa058b358f3e8981cbc
Tom Conroy <tom.conroy@firstlook.org>
2018-04-30 11:15:14 -0400

    fix babel-jest config?

--- scripts/utils/babelTransform.js
+++ scripts/utils/babelTransform.js
@@ -7,8 +7,8 @@ module.exports = babelJest.createTransformer({
     'babel-preset-react',
     'babel-preset-env',
     'babel-preset-stage-0',
-  ],
+  ].map(require.resolve),
   plugins: [
     'babel-plugin-transform-runtime',
-  ],
+  ].map(require.resolve),
 });
