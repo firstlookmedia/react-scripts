354e68695465c3bf9f3be72886b309821eb4ee76
Carl Licata <cjlicata@gmail.com>
2018-07-19 12:21:11 -0400

    Mocks .scss files as well when testing

--- scripts/utils/createJestConfig.js
+++ scripts/utils/createJestConfig.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = () => ({
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(__dirname, '__mocks__/file.js'),
-    '\\.(css)$': require.resolve('identity-obj-proxy'),
+    '\\.(css|scss)$': require.resolve('identity-obj-proxy'),
     '^react-relay/compat$': path.resolve(__dirname, '__mocks__/react-relay/compat.js'),
     '^react-relay/classic$': path.resolve(__dirname, '__mocks__/react-relay/classic.js'),
     '^react-relay$': path.resolve(__dirname, '__mocks__/react-relay.js'),
