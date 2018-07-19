5e7a4c06909ae33504473b5dda9fabd39b1a22dc
Carl Licata <cjlicata@gmail.com>
2018-07-19 13:18:23 -0400

    Specifies the Jest config as a path (inlined JSON didn't appear to be working)

--- scripts/utils/jestConfig.js
+++ scripts/utils/jestConfig.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-module.exports = () => ({
+module.exports = {
+  rootDir: process.cwd(),
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(__dirname, '__mocks__/file.js'),
     '\\.(css|scss)$': require.resolve('identity-obj-proxy'),
@@ -15,4 +16,4 @@ module.exports = () => ({
   testRegex: 'src/.*__spec\\.js$',
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
   setupFiles: [path.resolve(__dirname, 'testSetup.js')],
-});
+};
