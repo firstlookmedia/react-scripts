2342df9d981aa284ece8a17b4731c29aa5f8c536
Vincent Abruzzo <vgabruzzo@gmail.com>
2018-07-30 15:52:58 -0400

    edit build to use rcp

--- scripts/build.js
+++ scripts/build.js
@@ -60,7 +60,7 @@ function handler(config, err, stats) {
 }
 console.log('Compiling relay queries...');
 const relayCompiler = spawn(
-  path.resolve('./node_modules/.bin/relay-compiler'),
+  path.resolve('./node_modules/.bin/relay-compiler-plus'),
   relayCompilerArguments,
   { stdio: 'inherit' }
 );
