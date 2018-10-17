b36d35f4f085e2e8754c74f39c35224830247c74
jcseo3 <jseo@colgate.edu>
2018-10-17 17:14:39 -0400

    add cancelAF

--- scripts/utils/testSetup.js
+++ scripts/utils/testSetup.js
@@ -2,6 +2,8 @@ global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);
 };
 
+global.cancelAnimationFrame = () => clearTimeout;
+
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
