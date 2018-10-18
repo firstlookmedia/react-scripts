5c46dd5ea8b21b7cf763b4d78b3573be3e4a4b85
jcseo3 <jseo@colgate.edu>
2018-10-18 12:09:33 -0400

    update adapter v

--- scripts/utils/testSetup.js
+++ scripts/utils/testSetup.js
@@ -2,7 +2,7 @@ global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);
 };
 
-global.cancelAnimationFrame = () => clearTimeout;
+global.cancelAnimationFrame = () => {};
 
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
