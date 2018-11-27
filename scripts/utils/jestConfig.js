956109b279109e482e3d55c6f540cc77a10dc6cd
Vincent Abruzzo <vgabruzzo@gmail.com>
2018-11-27 15:03:59 -0500

    ts-jest support

--- scripts/utils/jestConfig.js
+++ scripts/utils/jestConfig.js
@@ -1,18 +1,24 @@
 const path = require('path');
+const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
   rootDir: process.cwd(),
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$': path.resolve(__dirname, '__mocks__/file.js'),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$': path.resolve(
+      __dirname,
+      '__mocks__/file.js',
+    ),
     '\\.(css|scss)$': require.resolve('identity-obj-proxy'),
     '^react-relay/compat$': path.resolve(__dirname, '__mocks__/react-relay/compat.js'),
     '^react-relay/classic$': path.resolve(__dirname, '__mocks__/react-relay/classic.js'),
     '^react-relay$': path.resolve(__dirname, '__mocks__/react-relay.js'),
   },
   transform: {
+    ...tsjPreset.transform,
     '\\.js$': path.resolve(__dirname, 'babelTransform.js'),
   },
-  collectCoverageFrom: ['src/**/*.js'],
+
+  collectCoverageFrom: ['src/**/*.tsx', '!src/**/*.stories.tsx'],
   testRegex: 'src/.*__spec\\.js$',
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
   setupFiles: [path.resolve(__dirname, 'testSetup.js')],
