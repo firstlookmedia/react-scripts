1c27aaeb61cc6df4e1d5c7a207ab19b7548a8245
Vincent Abruzzo <vgabruzzo@gmail.com>
2018-12-04 14:52:41 -0500

    add require context hook babel

--- scripts/utils/babelTransform.js
+++ scripts/utils/babelTransform.js
@@ -1,15 +1,13 @@
 const babelJest = require('babel-jest');
 const config = require('../../config/webpack.defaults.js');
 
-require('babel-plugin-require-context-hook/register')();
-
 module.exports = babelJest.createTransformer({
   passPerPreset: true,
   presets: ['babel-preset-react', 'babel-preset-env', 'babel-preset-stage-0'].map(require.resolve),
   plugins: ['babel-plugin-transform-runtime'].map(require.resolve),
   env: {
     test: {
-      plugins: ['require-context-hook'].map(require.resolve),
+      plugins: ['babel-plugin-require-context-hook'].map(require.resolve),
     },
   },
 });
