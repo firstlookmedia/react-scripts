b8e420735749d51e647d2abce3ca727e5c4a32a3
Tom Conroy <tom.conroy@firstlook.org>
2018-12-04 12:56:18 -0500

    rethink server-side hot code reloading

--- config/webpack.server.dev.js
+++ config/webpack.server.dev.js
@@ -1,5 +1,17 @@
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const StartServerPlugin = require('start-server-webpack-plugin');
 const defaults = require('./webpack.server');
 
 module.exports = Object.assign({}, defaults, {
   mode: 'development',
+  entry: ['webpack/hot/poll?1000'].concat(defaults.entry),
+  watch: true,
+  externals: [nodeExternals({
+    whitelist: ['webpack/hot/poll?1000'],
+  })],
+  plugins: defaults.plugins.concat(
+    new StartServerPlugin('server.js'),
+    new webpack.HotModuleReplacementPlugin(),
+  )
 });
