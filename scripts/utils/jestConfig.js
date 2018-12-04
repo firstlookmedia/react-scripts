3b71b22ba6d94ce82b20bc0c36b38bbea3cea149
Vincent Abruzzo <vgabruzzo@gmail.com>
2018-12-04 16:07:53 -0500

    add require context hook babel

--- scripts/utils/jestConfig.js
+++ scripts/utils/jestConfig.js
@@ -24,9 +24,4 @@ module.exports = {
   testRegex: 'src/.*__spec\\.(jsx?|tsx?)$',
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
   setupFiles: [path.resolve(__dirname, 'testSetup.js')],
-  globals: {
-    'ts-jest': {
-      useBabelrc: true,
-    },
-  },
 };
