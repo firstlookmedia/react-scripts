866e1767c7adc17bf7563e18d1e0a97138f9ada7
Adam Cooper <theadamcooper@protonmail.com>
2019-02-11 16:00:50 -0500

    Include various items needed by graphql-query-test-mock

--- scripts/utils/testSetup.js
+++ scripts/utils/testSetup.js
@@ -7,5 +7,7 @@ const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
+global.fetch = require('node-fetch');
+
 global.mount = Enzyme.mount;
 global.shallow = Enzyme.shallow;
