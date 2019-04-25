ce2b430d88cfe70f1abcdbaab52a7092ce64860c
Vincent Abruzzo <vgabruzzo@gmail.com>
2019-04-25 14:27:16 -0400

    add loadable components

--- config/webpack.client.dev.js
+++ config/webpack.client.dev.js
@@ -4,25 +4,36 @@ const path = require('path');
 const LoadablePlugin = require('@loadable/webpack-plugin');
 const defaults = require('./webpack.defaults');
 
-const config = merge.smart({
-  module: {
-    rules: [{
-      test: /\.css$/,
-      include: path.resolve('src'),
-      use: ['style-loader'],
-    }, {
-      test: /\.css$/,
-      include: path.resolve('node_modules'),
-      use: ['style-loader'],
-    }, {
-      test: /\.scss$/,
-      use: ['style-loader'],
-    }],
+const config = merge.smart(
+  {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include: path.resolve('src'),
+          use: ['style-loader'],
+        },
+        {
+          test: /\.css$/,
+          include: path.resolve('node_modules'),
+          use: ['style-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader'],
+        },
+      ],
+    },
   },
-}, defaults, {
-  devtool: 'eval-source-map',
-  entry: ['webpack-hot-middleware/client'],
-  plugins: [new webpack.HotModuleReplacementPlugin(), new LoadablePlugin()],
-});
+  defaults,
+  {
+    devtool: 'eval-source-map',
+    entry: ['webpack-hot-middleware/client'],
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new LoadablePlugin({ filename: 'stats.json', writeToDisk: true }),
+    ],
+  },
+);
 
 module.exports = config;
