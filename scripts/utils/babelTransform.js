193afcbafdb2f8f5dc1b59bd6e7e0c83320978e1
Vincent Abruzzo <vgabruzzo@gmail.com>
2019-04-25 16:34:36 -0400

    uodate babel transform

--- scripts/utils/babelTransform.js
+++ scripts/utils/babelTransform.js
@@ -3,6 +3,16 @@ const config = require('../../config/webpack.defaults.js');
 
 module.exports = babelJest.createTransformer({
   passPerPreset: true,
-  presets: ['babel-preset-react', 'babel-preset-env', 'babel-preset-stage-0'].map(require.resolve),
-  plugins: ['babel-plugin-transform-runtime'].map(require.resolve),
+  presets: ['@babel/preset-react', '@babel/preset-env'].map(require.resolve),
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-json-strings',
+    '@babel/plugin-proposal-function-sent',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-throw-expressions',
+  ].map(require.resolve),
 });
