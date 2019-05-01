0202ddf6b435d97853bc19f869b92b0266b68cc6
Vincent Abruzzo <vgabruzzo@gmail.com>
2019-05-01 17:33:03 -0400

    css test

--- config/webpack.client.prod.js
+++ config/webpack.client.prod.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const ManifestPlugin = require('webpack-manifest-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const merge = require('webpack-merge');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
@@ -33,19 +32,15 @@ const config = merge.smart(
   {
     mode: 'production',
     output: {
-      filename: '[hash].js',
+      filename: '[name]-bundle-[chunkhash:8].js',
     },
     plugins: [
       new LoadablePlugin({ filename: 'stats.json', writeToDisk: true }),
-      new ManifestPlugin({ fileName: 'manifest.json' }),
-      new ManifestPlugin({ fileName: `manifest.${Date.now()}.json` }),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"',
       }),
       new ProgressBarPlugin(),
-      new MiniCSSExtractPlugin({
-        filename: '[contenthash].css',
-      }),
+      new MiniCSSExtractPlugin(),
       new OptimizeCSSAssetsPlugin({
         cssProcessorOptions: {
           reduceIdents: false,
