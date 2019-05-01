c4da5a62a3155cddd5eae036ea4202fc9352bb5a
Vincent Abruzzo <vgabruzzo@gmail.com>
2019-05-01 17:40:30 -0400

    css test

--- config/webpack.client.prod.js
+++ config/webpack.client.prod.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const ManifestPlugin = require('webpack-manifest-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const merge = require('webpack-merge');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
@@ -36,6 +37,8 @@ const config = merge.smart(
     },
     plugins: [
       new LoadablePlugin({ filename: 'stats.json', writeToDisk: true }),
+      new ManifestPlugin({ fileName: 'manifest.json' }),
+      new ManifestPlugin({ fileName: `manifest.${Date.now()}.json` }),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"',
       }),
