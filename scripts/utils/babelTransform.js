8ab75af509656a1c3b18518f487f0bb8a60bd2f4
Vincent Abruzzo <vgabruzzo@gmail.com>
2019-05-02 14:54:49 -0400

    add plugins to babel transform

--- scripts/utils/babelTransform.js
+++ scripts/utils/babelTransform.js
@@ -14,5 +14,7 @@ module.exports = babelJest.createTransformer({
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-numeric-separator',
     '@babel/plugin-proposal-throw-expressions',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
   ].map(require.resolve),
 });
