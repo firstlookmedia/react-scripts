da7b4511b3f3f3b7551cb51357876fbd5d171547
Vincent Abruzzo <vgabruzzo@gmail.com>
2022-04-28 14:47:37 -0400

    bump

--- config/babelOptions.js
+++ config/babelOptions.js
@@ -44,6 +44,8 @@ const babelOptions = {
     '@babel/plugin-proposal-throw-expressions',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-for-of',
   ].filter(Boolean),
 };
 
